7f0ebcff4e36287fc1190ef7a548e120c0e505cd
Pamela Frieze <pfrieze@gmail.com>
2017-12-13 03:30:28 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,5 @@ class Person(:avi)
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
-end
 
 end
